bc6fb30c00c809180d43bec3e0f23a31e0b392e3
fdevinar <fc_devinar@hotmail.com>
2025-09-01 19:25:25 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>BOOGIE OF THE DEEP - development environment</h1>
+    </>
+  )
+}
+
+export default App
