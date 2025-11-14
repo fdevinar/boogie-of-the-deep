08e363aab123bcc47d08291ac5b4817f179ad1a8
fdevinar <fc_devinar@hotmail.com>
2025-11-14 18:29:57 -0300

    all room images and style tweak

--- src/rooms/rooms.js
+++ src/rooms/rooms.js
@@ -3,50 +3,55 @@ const rooms =
 
 {
     "Forecastle": {
-        "text": "You arrive at the forecastle, you feel this ships holds misteries.",     
-        "image": "src/assets/forecastle.png",   
-        "choices": [
-            {"label": "Go to Dining Room", "action": "MOVE", "target": "Dining Room",},
-            {"label": "Go to Store Room", "action": "MOVE", "target": "Store Room"},
+        text: "You arrive at the forecastle, you feel this ships holds misteries.",     
+        image: "src/assets/forecastle.png",   
+        choices: [
+            {label: "Go to Dining Room", action: "MOVE", target: "Dining Room",},
+            {label: "Go to Store Room", action: "MOVE", target: "Store Room"},
         ]           
     },
     "Dining Room": {
         text: "A ghoulish banquet has been held here aeons ago",
-        comment: "You're not sure moving to the next room is a good thing, you can't see where it leads.",
-        "choices": [
-            {"label": "Go to next room", "action": "MOVE", "target": "Lobby", "effect": {sanity: -20},},
+        comment: "You're not sure moving to the next room is a good thing.",
+        image: "src/assets/dining-room.png",           
+        choices: [
+            {label: "Go to Lobby", action: "MOVE", target: "Lobby", effect: {sanity: -20},},
         ]
     },
     "Lobby": {
         text: "The lobby is quiet, maybe too quiet",
         comment: "You start to doubt yourself",
-        "choices": [
-            {"label": "Play the piano", "action": "EVENT", "target": "playedPiano", 
-            "effect": {sanity: 10}, "message": "You hear an inspiring tune, a passage has opened" },
-            {"label": "Use the passage", "action": "MOVE", "target": "Forecastle", "conditions": {has:['playedPiano']}}
+        image: "src/assets/lobby.png",
+        choices: [
+            {label: "Play the piano", action: "EVENT", target: "playedPiano", 
+            effect: {sanity: 10}, message: "You hear an inspiring tune, a passage has opened" },
+            {label: "Use the passage", action: "MOVE", target: "Forecastle", conditions: {has:['playedPiano']}}
         ]
     },
     "Store Room": {
         text: "The store room reeks of stale air and bad memories",
-        "choices": [
-            {"label": "Go to Engine Room", "action": "MOVE", "target": "Engine Room"},
-            {"label": "Pickup the Wrench", "action": "PICKUP", "target": "Wrench",
-             "message": "You pick up the Wrench.. It feels heavy, like it could break apart soft wood."},
+        image: "src/assets/store-room.png",
+        choices: [
+            {label: "Go to Engine Room", action: "MOVE", target: "Engine Room"},
+            {label: "Pickup the Wrench", action: "PICKUP", target: "Wrench",
+             message: "You pick up the Wrench.. It feels heavy, like it could break apart soft wood."},
 
         ]
     },
     "Engine Room": {
         text: "The hum of the machines, a perfect mix of chaos, steam and steel...",
+        image: "src/assets/engine-room.png",
         comment: "You could use a tool to open a passage to another room",
-        "choices": [
-            {"label": "Go to Boiler Room", "action": "MOVE", "target": "Boiler Room",
-                "conditions": {has:['Wrench']},
-                "message": "You used the Wrench to open a passage to the Boiler Room",
+        choices: [
+            {label: "Go to Boiler Room", action: "MOVE", target: "Boiler Room",
+                conditions: {has:['Wrench']},
+                message: "You used the Wrench to open a passage to the Boiler Room",
             },
         ]
     },
     "Boiler Room": {
-        text: "The heart of the ship...",        
+        text: "The heart of the ship...",
+        image: "src/assets/boiler-room.png",   
     },
 }
 
